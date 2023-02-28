878f5e5512fe0163e0f69a7a382d682cecbd1af0
Conrado Lanusse <conrado.lanusse@gmail.com>
2023-02-28 20:58:54 -0300

    Clase 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+// import hola from './Utils.js'
+// import { saludar, chau } from './Utils.js'
 
 function App() {
-  
+
+  // saludar()
+  // chau()
+
+  const prof = "Mauro Paul"
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Conrado Lanusse (the best)
+          El profesor eso {prof}
         </p>
         <h2>Coderhouse inc.</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendamos react
-        </a>
       </header>
+
+      
     </div>
   );
 }
---
+++ src/Utils.js
@@ -0,0 +1,12 @@
+
+
+export const saludar = () => {
+    console.log("Hola mundo")
+}
+
+export const chau = () => {
+    console.log("Chau mundo")
+}
+
+
+export default saludar
